5871821c48bcf660a22e5355aee40df1c0eb2c77
Yura Pelehatiy <yuriypelehatiy.gs@gmail.com>
2018-11-19 17:32:55 +0200

    Some fixes

--- src/components/TextAreaForm/TextAreaForm.js
+++ src/components/TextAreaForm/TextAreaForm.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import s from './TextAreaForm.module.css'
+import classNames from 'classnames/bind';
+
+let cx = classNames.bind(s)
 
 const TextAreaForm = ({
     meta,
     ...props
 }) => (
     <div className={s.TextAreaContainer}>
-        <textarea {...props} className={s.TextAreaField} rows={20}/>
+        <textarea 
+            {...props} 
+            className={cx({ 
+                TextAreaField: true, 
+                TextAreaFieldError: (meta && meta.error && meta.touched),
+            })}
+            rows={20}/>
         {meta && meta.error && meta.touched && <span className={s.ErrorField}>{meta.error}</span>}
     </div>
 );

cf1a1056b09be141bf7b5b390b48f1778a39d142
Yura Pelehatiy <yuriypelehatiy.gs@gmail.com>
2018-11-21 23:31:34 +0200

    Index.js Route fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react'
 import { compose, lifecycle } from 'recompose';
@@ -15,7 +15,7 @@ const InitAppComponent = () => (
     <Provider store={store}>
         <PersistGate loading={<Loader />} persistor={persistor} >
             <BrowserRouter>
-                <Route component={App}/>
+                <App/>
             </BrowserRouter>
         </PersistGate>
     </Provider>

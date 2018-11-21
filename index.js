432ebec0ffda1890126f9d802167355c0e86363d
Yura Pelehatiy <yuriypelehatiy.gs@gmail.com>
2018-11-21 18:12:05 +0200

    make it better

--- index.js
+++ index.js
@@ -6,11 +6,8 @@ const proxy = require('http-proxy-middleware');
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/*', function(req, res) {
-    // Proxy
-    app.use(proxy('/api', { target: "https://internet-store-api.herokuapp.com", changeOrigin: true}));
-
-    // Express server
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    app.use(proxy('/api', { target: "https://internet-store-api.herokuapp.com", changeOrigin: true}));
 });
 
 app.listen(process.env.PORT);

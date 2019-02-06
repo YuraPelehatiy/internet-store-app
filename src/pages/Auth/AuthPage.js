90a80e3fc19cef109071a785df6555f7f77c46d3
Yura <yuriypelehatiy.gs@gmail.com>
2019-02-06 13:38:39 +0200

    Add eslint and change structure of auth.

--- src/pages/Auth/AuthPage.js
+++ src/pages/Auth/AuthPage.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { NavLink, Link, Route, Switch } from 'react-router-dom';
-import { routes } from '../../routes'
-import s from './AuthPage.module.css'
-import LoginPage from '../Login/LoginPage';
-import RegisterPage from '../Register/RegisterPage';
-import RememberPage from '../Remember/RememberPage';
+import {
+    NavLink, Link, Route, Switch,
+} from 'react-router-dom';
+import { routes } from '../../routes';
+import s from './AuthPage.module.css';
+import LoginPage from './Login/LoginPage';
+import RegisterPage from './Register/RegisterPage';
+import RememberPage from './Remember/RememberPage';
 
 const AuthPage = () => (
     <div className={s.AuthPage}>
@@ -14,18 +16,18 @@ const AuthPage = () => (
         <div className={s.AuthForm}>
             <ul className={s.AuthFormNav}>
                 <li className={s.AuthFormNavItem}>
-                    <NavLink 
-                        to={routes.authLogin} 
-                        className={s.AuthFormLink} 
+                    <NavLink
+                        to={routes.authLogin}
+                        className={s.AuthFormLink}
                         activeClassName={s.AuthFormActiveLink}
                     >
                         Login
                     </NavLink>
                 </li>
                 <li className={s.AuthFormNavItem}>
-                    <NavLink 
-                        to={routes.authRegister} 
-                        className={s.AuthFormLink} 
+                    <NavLink
+                        to={routes.authRegister}
+                        className={s.AuthFormLink}
                         activeClassName={s.AuthFormActiveLink}
                     >
                         Register
@@ -38,7 +40,7 @@ const AuthPage = () => (
                 <Route path={routes.authRemember} component={RememberPage} />
             </Switch>
             <div>
-                <Link to={routes.authRemember}>Forgot password?</Link> 
+                <Link to={routes.authRemember}>Forgot password?</Link>
             </div>
         </div>
     </div>
